24761769f8f96b9ba9c4e0a0d159dd4c700a6f49
rmc3408 <rmc3408@protonmail.com>
2021-02-02 10:41:13 -0500

    video 7 section2

--- 02/app.js
+++ 02/app.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+function add(n1, n2, isP, msg) {
+    var sum = n1 + n2;
+    if (isP) {
+        console.log(msg + sum);
+    }
+    else
+        console.log(msg + 'a text');
+}
+var number1;
+number1 = 5;
+var number2 = 2.8;
+var isNumber = true;
+var text = 'The result is ';
+add(number1, number2, isNumber, text);
--- 02/app.ts
+++ 02/app.ts
@@ -1,1 +1,14 @@
-console.log("hello world again");
+function add(n1: number, n2: number, isP: boolean, msg: string) {
+    const sum = n1 + n2;
+    if (isP) { console.log(msg + sum); }
+    else console.log(msg + 'a text');
+
+}
+
+let number1: number;
+number1 = 5;
+const number2 = 2.8;
+const isNumber = true;
+const text = 'The result is ';
+
+add(number1, number2, isNumber, text);

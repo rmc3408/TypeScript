b585e520658e9e37098226325cd5d78d033f9239
rmc3408 <rmc3408@protonmail.com>
2021-02-02 20:02:46 -0500

    types video 14

--- 02/app.js
+++ 02/app.js
@@ -1,3 +1,9 @@
+/*
+*
+*             Types
+*
+**/
+// types boolean, number , string
 function add(n1, n2, isP, msg) {
     var sum = n1 + n2;
     if (isP) {
@@ -12,3 +18,37 @@ var number2 = 2.8;
 var isNumber = true;
 var text = 'The result is ';
 add(number1, number2, isNumber, text);
+// types object and Arrays and tuples
+var person = {
+    name: 'Max',
+    age: 40,
+    hobbies: ['cooking', 'music'],
+    roles: [2, 'author'],
+    infoAuthor: ['Stephen', 43, 120.99] // tuple with 3 elements
+};
+console.log(person.age);
+var favoritesNum = [1, 2, 4];
+for (var num in favoritesNum) { //give the index
+    console.log(num);
+}
+for (var _i = 0, favoritesNum_1 = favoritesNum; _i < favoritesNum_1.length; _i++) { //give the value 
+    var num = favoritesNum_1[_i];
+    console.log(num);
+}
+var randomTxt = ['error', 22, true];
+console.log(randomTxt);
+person.roles[1] = 'S. King';
+person.roles[0] = 3409;
+console.log(person.roles);
+// enum
+var idRoles;
+(function (idRoles) {
+    idRoles[idRoles["ADMIN"] = 5] = "ADMIN";
+    idRoles[idRoles["SUPERVISOR"] = 6] = "SUPERVISOR";
+    idRoles[idRoles["USER"] = 7] = "USER";
+})(idRoles || (idRoles = {}));
+var myroles = {
+    id: idRoles.ADMIN
+};
+console.log('ADMIN is ', myroles.id);
+console.log('2 X USER is ', idRoles.USER * 2);
--- 02/app.ts
+++ 02/app.ts
@@ -1,3 +1,12 @@
+/*
+*
+*             Types
+*
+*      number, string, boolean, array, tuple, object, enum, any
+**/
+
+
+// types boolean, number , string
 function add(n1: number, n2: number, isP: boolean, msg: string) {
     const sum = n1 + n2;
     if (isP) { console.log(msg + sum); }
@@ -12,3 +21,58 @@ const isNumber = true;
 const text = 'The result is ';
 
 add(number1, number2, isNumber, text);
+
+
+// types object and Arrays and tuples
+
+const person: { 
+    id: any,
+    name: string;
+    age: number;
+    hobbies: string[];
+    roles: [number, string]; // tuple array with fixed-size elements. 
+    infoAuthor: [string, number, number];
+
+} = {  // : object
+    id: 'anyCode',
+    name: 'Max',
+    age: 40,
+    hobbies: ['cooking', 'music'],
+    roles: [2, 'author'],  // tuple with 2 elements - number, string.
+    infoAuthor: ['Stephen', 43, 120.99] // tuple with 3 elements
+
+};
+
+console.log(person.age);
+
+let favoritesNum: number[] = [1, 2, 4];
+
+for (const num in favoritesNum) { //give the index
+    console.log(num);
+}
+for (const num of favoritesNum) { //give the value 
+    console.log(num);
+}
+
+let randomTxt: any[] = ['error', 22, true];
+console.log(randomTxt);
+
+person.roles[1] = 'S. King';
+person.roles[0] = 3409;
+
+console.log(person.roles);
+
+
+// enum
+
+enum idRoles { ADMIN = 5, SUPERVISOR, USER }
+
+const myroles = {
+    id: idRoles.ADMIN
+};
+
+console.log('ADMIN is ', myroles.id);
+console.log('2 X USER is ', idRoles.USER * 2);
+
+
+// 

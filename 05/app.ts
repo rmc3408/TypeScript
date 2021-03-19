db3dead966d7f9cbf9bf0d1d7c3e8fa439668ecd
rmc3408 <rmc3408@protonmail.com>
2021-03-19 16:34:25 -0400

    video 5 - classes

--- 05/app.ts
+++ 05/app.ts
@@ -5,11 +5,32 @@
  */
 
 class Department {
+    
     name: string;
 
     constructor(n: string) {
         this.name = n;
     }
+
+    describe(this: Department) {
+        console.log('Department: '+ this.name)
+    }
 }
 
+const acc = new Department("financial");
+console.log(acc)
+acc.describe();
 
+/**
+ * Use describe metho from Department as pointing reference.
+ * Calling this method; this.name will look in acc2 object
+ * need to have name.
+ * 
+ * it must have same name as department class and object key
+ */
+const acc2 = {
+    name: 'Warehouse',
+    describe: acc.describe 
+}
+
+acc2.describe();

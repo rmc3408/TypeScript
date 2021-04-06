6d9c186c4b21dabceefa4d7fd02f30ccaabca57d
rmc3408 <rmc3408@protonmail.com>
2021-04-06 12:28:13 -0400

    video 5 done

--- 07/dist/generics.js
+++ 07/dist/generics.js
@@ -1,9 +1,9 @@
 "use strict";
 const names1 = ['r', 'b', 'c'];
-const names2 = [];
+names1[0].split('');
+const names2 = [1, 2, 3];
 const names3 = [];
 const names4 = [];
-names1[0].split('');
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
         resolve('this is results');
@@ -15,9 +15,27 @@ function merge(a, b, c, d) {
     b.toFixed(2);
     return Object.assign(c, d);
 }
-const merged1 = merge('Max', 30, 30, 30);
+const merged1 = merge('Max', 30, 30, 40);
 const merged2 = merge('Max', 30, [1, 2, 3], 'sapo');
-const merged3 = merge('Max', 30, { name: 'R' }, { age: '39' });
-merged1.toFixed();
+const merged3 = merge('Max', 30, { name: 'R' }, { age: 39 });
 merged2.push();
-merged3.age;
+merged3.age = 23;
+console.log(merged3);
+//give any structure of object
+function joint(c, d) {
+    return Object.assign(c, d);
+}
+const merged4 = joint({ name: 'R' }, { age: 39 });
+const merged5 = joint({ name: 'R' }, 39);
+console.log(merged4);
+console.log(merged5);
+function countPrint(c) {
+    let description = 'no values';
+    if (c.length !== 0) {
+        description = 'Got ' + c.length + ' elements.';
+    }
+    return [c, description];
+}
+console.log(countPrint('hello'));
+console.log(countPrint([1, 2, 3]));
+console.log(countPrint(''));
--- 07/generics.ts
+++ 07/generics.ts
@@ -1,8 +1,9 @@
 const names1: Array<string> = ['r', 'b', 'c'];
-const names2: Array<string> = [];
+names1[0].split('');
+const names2: Array<number> = [1,2,3];
 const names3: string[] = [];
 const names4: Array<string | number> = [];
-names1[0].split('');
+
 
 const promise: Promise<string> = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -13,19 +14,46 @@ promise.then(data => console.log(data.toUpperCase()));
 
 
 
-function merge<T, S>( a: string, b: number, c: T, d: S) {
+function merge<T, S>(a: string, b: number, c: T, d: S) {
     a.toUpperCase();
     b.toFixed(2);
-    return Object.assign(c,d);
+    return Object.assign(c, d);
 }
 
-const merged1 = merge('Max', 30, 30, 30);
+const merged1 = merge<number, number>('Max', 30, 30, 40);
 const merged2 = merge('Max', 30, [1,2,3], 'sapo');
-const merged3 = merge('Max', 30, { name: 'R' }, { age: '39' });
+const merged3 = merge('Max', 30, { name: 'R' }, { age: 39 });
 
-merged1.toFixed();
 merged2.push();
-merged3.age;
+merged3.age = 23;
+
+console.log(merged3);
+
+//give any structure of object
+function joint<T extends object | string, S extends object | number>(c: T, d: S) {
+    return Object.assign(c, d);
+}
+const merged4 = joint({ name: 'R' }, { age: 39 });
+const merged5 = joint({ name: 'R' }, 39);  
+
+console.log(merged4);
+console.log(merged5);
 
 
 
+
+function countPrint<T extends wordLen>(c: T): [T, string] {
+    let description = 'no values';
+    if (c.length !== 0) {
+        description = 'Got ' + c.length + ' elements.';
+    }
+    return [c, description];
+}
+
+interface wordLen {
+    length: number;
+}
+
+console.log(countPrint('hello'));
+console.log(countPrint([1, 2, 3]));
+console.log(countPrint(''));
